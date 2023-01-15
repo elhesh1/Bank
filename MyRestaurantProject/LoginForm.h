9dc65b5962654df712d997ef2a9afd4fde2f6901
elhesh1 <ehs4@illinoi.edu>
2023-01-15 13:01:09 -0600

    Finished Admin features

--- MyRestaurantProject/LoginForm.h
+++ MyRestaurantProject/LoginForm.h
@@ -9,7 +9,7 @@ namespace MyRestaurantProject {
 
 	using namespace System;
 	using namespace System::ComponentModel;
-	using namespace System::Collections;
+	using namespace System::Collections;   
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
